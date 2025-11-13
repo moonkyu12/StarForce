4f2a111d2153a86ef3c11756d6432784a5af9bb3
moonkyu12 <choimoonkyu@icloud.com>
2025-11-14 00:04:59 +0900

    Initial commit1

---
+++ src/main/java/com/example/starForce/StarForce.java
@@ -0,0 +1,17 @@
+package com.example.starForce;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StarForce extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
