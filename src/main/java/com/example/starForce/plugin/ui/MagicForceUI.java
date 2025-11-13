5cd2f808164e71606ec7bd1da115dbbb410fcaa8
moonkyu12 <choimoonkyu@icloud.com>
2025-11-14 02:20:31 +0900

    D 이벤트핸들럼나듬

--- src/main/java/com/example/starForce/plugin/ui/MagicForceUI.java
+++ src/main/java/com/example/starForce/plugin/ui/MagicForceUI.java
@@ -7,7 +7,7 @@ import org.bukkit.inventory.Inventory;
 public class MagicForceUI {
 
     public static void openMagicForceUI(Player player) {
-        Inventory ui = Bukkit.createInventory(null, 27, "MagicForce UI");
+        Inventory ui = Bukkit.createInventory(null, 27, "스타★포스 인벤토리");
         player.openInventory(ui);
     }
 }
