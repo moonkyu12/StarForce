4bdf2c7229cce3952903b394d0f3810bfdcb6326
moonkyu12 <choimoonkyu@icloud.com>
2025-11-14 01:40:54 +0900

    D 이벤트핸들럼나듬

--- src/main/java/com/example/starForce/plugin/StarForce.java
+++ src/main/java/com/example/starForce/plugin/StarForce.java
@@ -1,6 +1,5 @@
-package com.example.starForce;
+package com.example.starForce.plugin;
 
-import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class StarForce extends JavaPlugin {
